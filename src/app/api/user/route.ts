eeb971ba73ecad43e3165387a4c6af712a77e5f4
Jon <jonmartirosyan1988@gmail.com>
2023-11-14 05:33:46 +0400

    update

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -6,6 +6,16 @@ export const POST = async (req: Request) => {
   return new Response(JSON.stringify(user))
 }
 export const GET = async (req: Request) => {
-  const users = await prisma.user.findMany()
+    const users = await prisma.user.findMany({
+      where: {
+        name: {
+          contains: "a",
+          startsWith: "s",
+        },
+      },
+      include: {
+        posts: true,
+      },
+    })
   return new Response(JSON.stringify(users))
 }
